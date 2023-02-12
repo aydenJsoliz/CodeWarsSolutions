3ff190c86b2c3bee0e52ac73b801a7e5668b4909
aydenJsoliz <asoliz7@mail.stmarytx.edu>
2023-02-12 13:29:00 -0600

    test

---
+++ 8 kyu/ThirdAngleOfATriangle.cpp
@@ -0,0 +1,16 @@
+/* 
+You are given two interior angles (in degrees) of a triangle.
+
+Write a function to return the 3rd.
+
+Note: only positive integers will be tested.
+*/
+
+class Triangle {
+public:
+  static int otherAngle(int a, int b);
+};
+
+int Triangle::otherAngle(int a, int b) {
+  return 180 - a - b;
+}
